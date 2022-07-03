6c11b06c255b62d0f2074291c21f3c2f002826be
unknown <sadkey17@gmail.com>
2022-07-03 22:10:13 +0200

    update in sigin and siginup

--- src/app/View/layout/layout.module.ts
+++ src/app/View/layout/layout.module.ts
@@ -7,8 +7,11 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { BaseComponent } from './base/base.component';
-
-
+import { PerfectScrollbarConfigInterface, PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 @NgModule({
   declarations: [
     LayoutComponent,
@@ -19,7 +22,14 @@ import { BaseComponent } from './base/base.component';
   ],
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    LayoutRoutingModule,
+    PerfectScrollbarModule
+  ],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
   ]
 })
 export class LayoutModule { }
